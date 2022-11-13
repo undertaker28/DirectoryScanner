a08d9c8840c28f1fb94e2068d48730a98b419932
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-11-13 15:32:22 +0300

    Add Models in Core

---
+++ Core/Models/FileSystemNode.cs
@@ -0,0 +1,26 @@
+﻿namespace Core.Models
+{
+    public class FileSystemNode
+    {
+        public string FullName { get; }
+        public string Name { get; }
+        public long Length { get; set; }
+        public List<FileSystemNode>? Children { get; set; }
+
+        public FileSystemNode(string fullName, string name)
+        {
+            FullName = fullName;
+            Name = name;
+        }
+
+        public FileSystemNode(string fullName, string name, long length) : this(fullName, name)
+        {
+            Length = length;
+        }
+
+        public FileSystemNode(string fullName, string name, List<FileSystemNode>? children) : this(fullName, name)
+        {
+            Children = children;
+        }
+    }
+}
---
+++ Core/Models/FileSystemTree.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.Models
+{
+    public class FileSystemTree
+    {
+        public FileSystemNode Root { get; }
+
+        public FileSystemTree(FileSystemNode root)
+        {
+            Root = root;
+            Root.Length = GetNodeLength(root);
+        }
+
+        public static long GetNodeLength(FileSystemNode node)
+        {
+            if (node.Children is null)
+            {
+                return node.Length;
+            }
+
+            foreach (FileSystemNode child in node.Children)
+            {
+                node.Length += GetNodeLength(child);
+            }
+
+            return node.Length;
+        }
+    }
+}
