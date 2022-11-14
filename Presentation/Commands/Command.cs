5ae51ae454972ab8e73e01ca79e3607c8bb0f91b
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-11-14 20:54:50 +0300

    Add Commands in Presentation

---
+++ Presentation/Commands/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Presentation.Commands
+{
+    public class Command : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object>? canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Command(Action<object> execute, Predicate<object>? canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute is null || canExecute(parameter!);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter!);
+        }
+    }
+}
