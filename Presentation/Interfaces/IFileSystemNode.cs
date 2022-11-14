8f8f53bfe1e8ba23f913ced34c656a7de70150fe
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-11-14 12:28:59 +0300

    Add Interfaces in Presentation

---
+++ Presentation/Interfaces/IFileSystemNode.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.Interfaces
+{
+    public interface IFileSystemNode
+    {
+        public string Name { get; }
+        public long Length { get; }
+        public double RelativeSize { get; }
+    }
+}
